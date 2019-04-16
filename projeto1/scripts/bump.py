02bb1f056ff06bdc01fe2340090e92286b6f7ca2
BrunoSDomingues <brunao.signorelli@hotmail.com>
2019-04-16 17:13:11 -0300

    Bumper corrigido

--- projeto1/scripts/bump.py
+++ projeto1/scripts/bump.py
@@ -6,96 +6,78 @@ from geometry_msgs.msg import Twist, Vector3
 from std_msgs.msg import UInt8
 from math import radians 
 
-v = 1.0
 w1 = radians(60)
-w2 = radians(45)
-vel_normal = 2.0
+vel_normal = 0.1
 bump = 0
-
-def bumped(data):
+vel_flee = 0.26
+def bumped(dado):
     global bump
 
-    bump = data.data
+    bump = dado.data
+    print(bump)
 
 if __name__ == "__main__":
 
-    rospy.init_node("bumper")
-    pub = rospy.Publisher("cmd_vel", Twist, queue_size=3)
+    rospy.init_node("batida")
+    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=3)
     bumper_number = rospy.Subscriber("/bumper", UInt8, bumped)
-
+    
     try:
         while not rospy.is_shutdown():
-            backwards = Twist(Vector3(-v,0,0),Vector3(0,0,0))
-            forwards = Twist(Vector3(v,0,0),Vector3(0,0,0))
+            backwards = Twist(Vector3(-vel_normal,0,0),Vector3(0,0,0))
             left = Twist(Vector3(0,0,0),Vector3(0,0,-w1))
             right = Twist(Vector3(0,0,0),Vector3(0,0,w1))
             stop = Twist(Vector3(0,0,0),Vector3(0,0,0))
 
             if bump == 1:
-                print("Backwards")
+                print("I have bumped into something.")
+
+                print("Going backwards...")
                 pub.publish(backwards)
                 rospy.sleep(1.5)
 
-                print("Stopped")
+                print("I stopped.")
                 pub.publish(stop)
                 rospy.sleep(1.0)
 
-                print("Turning right")
-                pub.publish(right)
+                print("Avoiding object...")
+                pub.publish(left)
                 rospy.sleep(1.5)
 
-            elif bump == 2:
-                print("Backwards")
+                bump = None
+
+            if bump == 2:
+                print("I have bumped into something.")
+
+                print("Going backwards...")
                 pub.publish(backwards)
                 rospy.sleep(1.5)
 
-                print("Stopped")
+                print("I stopped.")
                 pub.publish(stop)
                 rospy.sleep(1.0)
 
-                print("Turning left")
-                pub.publish(left)
-                rospy.sleep(1.5)
-
-            elif bump == 3:
-                print("Forwards")
-                pub.publish(forwards)
-                rospy.sleep(1.5)
-
-                print("Stopped")
-                pub.publish(stop)
-                rospy.sleep(1.0)
-
-                print("Turning right")
+                print("Avoiding object...")
                 pub.publish(right)
-                rospy.sleep(1.5) 
-
-            elif bump == 4:
-                print("Forwards")
-                pub.publish(forwards)
                 rospy.sleep(1.5)
 
-                print("Stopped")
-                pub.publish(stop)
-                rospy.sleep(1.0)
+                bump = None
 
-                print("Turning left")
-                pub.publish(left)
-                rospy.sleep(1.5)
+            if bump == 3 or bump == 4:
+                flee = Twist(Vector3(vel_flee,0,0),Vector3(0,0,0))
+                print("Something bumped into me?")
+
+                print("Running away!")
+                pub.publish(flee)
+                rospy.sleep(4.0)
+
+                bump = None
                     
             else:
-                print("Going")
+                print("Going normally.")
                 go_lin = Twist(Vector3(vel_normal,0,0),Vector3(0,0,0))
                 pub.publish(go_lin)
-                rospy.sleep(3.0)
+                rospy.sleep(0.1)
 
-                print("Stopped")
-                pub.publish(stop)
-                rospy.sleep(1.0)
-
-                print("Turning")
-                turn = Twist(Vector3(0,0,0),Vector3(0,0,w2))
-                pub.publish(turn)
-                rospy.sleep(1.0)
     except rospy.ROSInterruptException:
-        print("Ocorreu uma exceção com o rospy")
+        print("ROS.exe has stopped working.")
